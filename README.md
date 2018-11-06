bb07522e37cff03a8d1128e420073f5682a7138b
ken <block24block@gmail.com>
2018-11-06 23:44:54 +0800

    remove debug and add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# chuansong-all-posts
+
+[install](https://greasyfork.org/zh-CN/scripts/374040-chuansong-all-posts) to load all posts on http://chuansong.me/account/*
--- chuansong_all_posts.js
+++ chuansong_all_posts.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         chuansong-all-posts
 // @namespace    https://github.com/turnon/chuansong_all_posts
-// @version      0.0.1
+// @version      0.0.2
 // @description  auto load posts on chuansong.me
 // @author       block24block@gmail.com
 // @match        http://chuansong.me/account/*
 // @grant        none
 // @require      https://greasyfork.org/scripts/372188-ateles/code/ateles.js?version=631434
 // ==/UserScript==
-eval(await fetch('https://raw.githubusercontent.com/turnon/ateles/master/ateles.js').then(d => d.text()))
 Ateles(['page_loader'], function (page_loader) {
     page_loader({
         next_page: function (doc) {
